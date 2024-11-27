aa74a8dbe1d7c98b6ed6fe2d61ec6e5e0e74fe3b
Tommi <tommi.savolainen79@gmail.com>
2024-11-27 11:28:24 +0200

    Testausta

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,25 +1,20 @@
-import { ApolloClient, InMemoryCache, HttpLink, } from '@apollo/client';
-
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/graphql', 
-  fetch: (uri, options = {}) => {
-    const token = sessionStorage.getItem("mutatedToken"); 
+    uri: 'http://localhost:3000/graphql',
+    fetch: (uri, options = {}) => {
+        const token = sessionStorage.getItem('mutatedToken');
 
-    
-    options.headers = {
-      ...options.headers,
-      Authorization: token ? `Bearer ${token}` : "", 
-    };
+        options.headers = {
+            ...options.headers,
+            Authorization: token ? `Bearer ${token}` : '',
+        };
 
-    return fetch(uri, options);
-  },
+        return fetch(uri, options);
+    },
 });
-
 
 export const client = new ApolloClient({
-  link: httpLink, 
-  cache: new InMemoryCache(), 
+    link: httpLink,
+    cache: new InMemoryCache(),
 });
-
-
