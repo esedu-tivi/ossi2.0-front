1ced6f83590f830964ed0f228d53e56d791ea5dc
JoelTurtiainen <joel.turtiainen@gmail.com>
2026-01-13 12:36:36 +0200

    what even is this? hopefully not needed

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,6 @@ const App = () => {
     });
 
     useEffect(() => {
-        if (isAuthenticated) {
-            refetch();
-        }
-    }, [isAuthenticated, refetch]);
-
-    useEffect(() => {
         if (!isAuthenticated) return;
 
         if (!studentLoading && studentData) {
