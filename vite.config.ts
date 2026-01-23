fef0d496f61e3a0dd392662ae476beffb7fa78f0
JoelTurtiainen <joel.turtiainen@gmail.com>
2026-01-23 09:07:23 +0200

    feat: init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    host: true,          // Allow external connections (necessary for Docker)
-    port: 5174,          
+    host: true, // Allow external connections (necessary for Docker)
+    port: 5174,
     watch: {
-      usePolling: true,  // Use polling for file changes (helps with Docker)
+      usePolling: true, // Use polling for file changes (helps with Docker)
     },
   },
 });
-
